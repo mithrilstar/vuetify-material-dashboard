a7bb8f2bbd351a2029904af8cb70f2b868c98580
mithrilstar@gmail.com <mithrilstar@gmail.com>
2019-09-13 21:12:27 +0530

    couldn't get rid of linting errors till this

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': 'vuetify',
+  // plugins: ["es-beautifier"],
+  extends: [
+    "vuetify",
+    "plugin:vue/essential",
+    // "plugin:es-beautifier/standard"
+  ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": ["error", "never"],
+    "linebreak-style": "off"
   },
   parserOptions: {
     parser: 'babel-eslint'
